9340c81cdae1e5474a6af32b26e980b81b16bf9a
Konstantin Simon Maria Möllers <ksm.moellers@gmail.com>
2018-03-15 18:56:48 +0100

    Add a processor for HTML files

---
+++ Processor/HtmlProcessor.php
@@ -0,0 +1,198 @@
+<?php
+
+namespace Baqend\Component\Spider\Processor;
+
+use Baqend\Component\Spider\Asset;
+use Baqend\Component\Spider\Queue\QueueInterface;
+use Baqend\Component\Spider\UrlHelper;
+use Symfony\Component\DomCrawler\Crawler;
+
+/**
+ * This processor finds URLs in HTML, makes them absolute, and adds
+ * them to the processing queue.
+ *
+ * Class HtmlProcessor created on 2018-03-14.
+ *
+ * @author  Konstantin Simon Maria Möllers
+ * @package Baqend\Component\Spider\Processor
+ */
+class HtmlProcessor implements ProcessorInterface
+{
+
+    private static $matchTags = [
+        'a'            => ['href', 'urn'],
+        'amp-img'      => ['src', 'srcset'],
+        'base'         => ['href'],
+        'form'         => ['action', 'data'],
+        'img'          => ['src', 'usemap', 'longdesc', 'dynsrc', 'lowsrc', 'srcset'],
+        'link'         => ['href'],
+
+        'applet'       => ['code', 'codebase', 'archive', 'object'],
+        'area'         => ['href'],
+        'body'         => ['background', 'credits', 'instructions', 'logo'],
+        'input'        => ['src', 'usemap', 'dynsrc', 'lowsrc', 'action', 'formaction'],
+
+        'blockquote'   => ['cite'],
+        'del'          => ['cite'],
+        'frame'        => ['longdesc', 'src'],
+        'head'         => ['profile'],
+        'iframe'       => ['longdesc', 'src'],
+        'ins'          => ['cite'],
+        'object'       => ['archive', 'classid', 'codebase', 'data', 'usemap'],
+        'q'            => ['cite'],
+        'script'       => ['src'],
+
+        'audio'        => ['src'],
+        'command'      => ['icon'],
+        'embed'        => ['src', 'code', 'pluginspage'],
+        'event-source' => ['src'],
+        'html'         => ['manifest', 'background', 'xmlns'],
+        'source'       => ['src'],
+        'video'        => ['src', 'poster'],
+
+        'bgsound'      => ['src'],
+        'div'          => ['href', 'src'],
+        'ilayer'       => ['src'],
+        'layer'        => ['src'],
+        'table'        => ['background'],
+        'td'           => ['background'],
+        'th'           => ['background'],
+        'xml'          => ['src'],
+
+        'button'       => ['action', 'formaction'],
+        'datalist'     => ['data'],
+        'select'       => ['data'],
+
+        'access'       => ['path'],
+        'card'         => ['onenterforward', 'onenterbackward', 'ontimer'],
+        'go'           => ['href'],
+        'option'       => ['onpick'],
+        'template'     => ['onenterforward', 'onenterbackward', 'ontimer'],
+        'wml'          => ['xmlns'],
+    ];
+
+    /**
+     * @var CssProcessor|null
+     */
+    private $cssProcessor;
+
+    /**
+     * HtmlProcessor constructor.
+     *
+     * @param CssProcessor|null $cssProcessor
+     */
+    public function __construct(CssProcessor $cssProcessor = null) {
+        $this->cssProcessor = $cssProcessor;
+    }
+
+    /**
+     * Checks whether this asset can be processed.
+     *
+     * @param Asset $asset
+     * @return boolean True, if the given asset can be processed by this processor.
+     */
+    public function canProcess(Asset $asset) {
+        return strpos($asset->getContentType(), 'html') !== false || substr($asset->getUrl(), -5) === '.html';
+    }
+
+    /**
+     * Processes a given asset.
+     *
+     * Discovered new assets are written to the provided queue.
+     *
+     * @param Asset $asset The asset to be processed.
+     * @param QueueInterface $queue The queue to add new files to.
+     * @return Asset The processed asset.
+     */
+    public function process(Asset $asset, QueueInterface $queue) {
+        // Create DOM of asset's HTML
+        $domDocument = new \DOMDocument();
+        if (!$domDocument->loadHTML($asset->getContent())) {
+            return $asset;
+        }
+
+        // Find URLs in HTML document
+        foreach (self::$matchTags as $tag => $attributes) {
+            $elements = $domDocument->getElementsByTagName($tag);
+
+            foreach ($elements as $element) {
+                $this->processElement($asset, $element, $attributes, $queue);
+            }
+        }
+
+        // Find URLs in CSS style-tags
+        if ($this->cssProcessor !== null) {
+            // Handle 'style' tags
+            $crawler = new Crawler($domDocument);
+            $elements = $crawler->filterXPath('//*[@style]');
+            foreach ($elements as $element) {
+                $cssString = $element->getAttribute('style');
+                $cssString = $this->cssProcessor->processCss($asset, $cssString, $queue);
+
+                $element->setAttribute('style', $cssString);
+            }
+        }
+
+        return $asset->withContent($domDocument->saveHTML());
+    }
+
+    /**
+     * Processes URLs of an HTML element.
+     *
+     * @param Asset $asset The asset to be processed.
+     * @param \DOMElement $element The HTML element to be processed.
+     * @param string[] $attributes An array of attribute names to check.
+     * @param QueueInterface $queue The queue to add new files to.
+     */
+    private function processElement(Asset $asset, \DOMElement $element, $attributes, QueueInterface $queue) {
+        foreach ($attributes as $attribute) {
+            if ($element->hasAttribute($attribute)) {
+                $this->processElementAttribute($asset, $element, $attribute, $queue);
+            }
+        }
+    }
+
+    /**
+     * Processes URLs of an HTML element attribute.
+     *
+     * @param Asset $asset The asset to be processed.
+     * @param \DOMElement $element The HTML element to be processed.
+     * @param string $attribute Name of the attribute to check.
+     * @param QueueInterface $queue The queue to add new files to.
+     */
+    private function processElementAttribute(Asset $asset, \DOMElement $element, $attribute, QueueInterface $queue) {
+        if ($attribute === 'srcset') {
+            $this->processElementSrcSet($asset, $element, $queue);
+            return;
+        }
+
+        $currentValue = $element->getAttribute($attribute);
+        $updatedValue = UrlHelper::resolve($asset->getUrl(), $currentValue);
+        $queue->add(UrlHelper::stripFragment($updatedValue)[0]);
+
+        $element->setAttribute($attribute, $updatedValue);
+    }
+
+    /**
+     * Processes the "srcset" attribute of an HTML element.
+     *
+     * @param Asset $asset The HTML document's asset.
+     * @param \DOMElement $element The element being processed.
+     * @param QueueInterface $queue The queue to add URLs to process to.
+     */
+    private function processElementSrcSet(Asset $asset, \DOMElement $element, QueueInterface $queue) {
+        $value = $element->getAttribute('srcset');
+
+        $srcset = [];
+        preg_match_all('/([^"\'\s,]+)\s*\s+(\d+[wx])(?:,\s*)?/', $value, $sources, PREG_SET_ORDER);
+        foreach ($sources as $source) {
+            list(, $url, $config) = $source;
+            $url = UrlHelper::resolve($asset->getUrl(), UrlHelper::stripFragment($url)[0]);
+            $queue->add($url);
+
+            $srcset[] = "$url $config";
+        }
+
+        $element->setAttribute('srcset', implode(', ', $srcset));
+    }
+}
---
+++ Tests/Processor/HtmlProcessorTest.php
@@ -0,0 +1,172 @@
+<?php
+
+namespace Baqend\Component\Spider\Tests\Processor;
+
+use Baqend\Component\Spider\Asset;
+use Baqend\Component\Spider\Processor\CssProcessor;
+use Baqend\Component\Spider\Processor\HtmlProcessor;
+use Baqend\Component\Spider\Queue\BreadthQueue;
+use Baqend\Component\Spider\Tests\DummyAsset;
+use PHPUnit\Framework\TestCase;
+
+class HtmlProcessorTest extends TestCase
+{
+
+    /**
+     * @var BreadthQueue
+     */
+    private $queue;
+
+    /**
+     * @var CssProcessor
+     */
+    private $cssProcessor;
+
+    /**
+     * @var HtmlProcessor
+     */
+    private $htmlProcessor;
+
+    protected function setUp() {
+        $this->cssProcessor = new CssProcessor();
+        $this->htmlProcessor = new HtmlProcessor($this->cssProcessor);
+        $this->queue = new BreadthQueue();
+    }
+
+    /**
+     * @test
+     */
+    public function canProcessTextHtml() {
+        $processor = $this->htmlProcessor;
+
+        $this->assertTrue($processor->canProcess(new DummyAsset('https://example.org/index.html', 200, 'text/html')));
+        $this->assertTrue($processor->canProcess(new DummyAsset('https://example.org/index.html', 200, null)));
+        $this->assertTrue($processor->canProcess(new DummyAsset('https://example.org/index.html', 200, 'text/html; charset=utf-8')));
+    }
+
+    /**
+     * @test
+     */
+    public function cantProcessOther() {
+        $processor = $this->htmlProcessor;
+
+        $this->assertFalse($processor->canProcess(new DummyAsset('https://example.org/style.css', 200, 'text/css')));
+        $this->assertFalse($processor->canProcess(new DummyAsset('https://example.org/main.js', 200, 'application/javascript')));
+        $this->assertFalse($processor->canProcess(new DummyAsset('https://example.org/main.js', 200, 'application/javascript')));
+        $this->assertFalse($processor->canProcess(new DummyAsset('https://example.org/style.css', 200, null)));
+        $this->assertFalse($processor->canProcess(new DummyAsset('https://example.org/style.css', 200, 'text/css; charset=utf-8')));
+        $this->assertFalse($processor->canProcess(new DummyAsset('https://example.org/index.svg', 200, null)));
+        $this->assertFalse($processor->canProcess(new DummyAsset('https://example.org/index.svg', 200, 'text/xml+svg')));
+    }
+
+    /**
+     * @test
+     */
+    public function processLink() {
+        $processor = $this->htmlProcessor;
+        $queue = $this->queue;
+
+        $asset = $this->createAsset('/subdir/index.html', $this->htmlHead('<link href="/styles/main.css">'));
+        $queue->add($asset->getUrl());
+        $html = $processor->process($asset, $queue);
+
+        $this->assertEquals($this->htmlHead('<link href="https://example.org/styles/main.css">'), $html->getContent());
+        $this->assertEquals('https://example.org/subdir/index.html', $queue->next());
+        $this->assertEquals('https://example.org/styles/main.css', $queue->next());
+        $this->assertNull($queue->next());
+    }
+
+    /**
+     * @test
+     */
+    public function processScript() {
+        $processor = $this->htmlProcessor;
+        $queue = $this->queue;
+
+        $asset = $this->createAsset('/subdir/index.html', $this->htmlHead('<script src="/scripts/main.js"></script>'));
+        $queue->add($asset->getUrl());
+        $html = $processor->process($asset, $queue);
+
+        $this->assertEquals($this->htmlHead('<script src="https://example.org/scripts/main.js"></script>'), $html->getContent());
+        $this->assertEquals('https://example.org/subdir/index.html', $queue->next());
+        $this->assertEquals('https://example.org/scripts/main.js', $queue->next());
+        $this->assertNull($queue->next());
+    }
+
+    /**
+     * @test
+     */
+    public function processAnchor() {
+        $processor = $this->htmlProcessor;
+        $queue = $this->queue;
+
+        $asset = $this->createAsset('/subdir/index.html', $this->htmlBody('<a href="next/index.html">Hello World</a>'));
+        $queue->add($asset->getUrl());
+        $html = $processor->process($asset, $queue);
+
+        $this->assertEquals($this->htmlBody('<a href="https://example.org/subdir/next/index.html">Hello World</a>'), $html->getContent());
+        $this->assertEquals('https://example.org/subdir/index.html', $queue->next());
+        $this->assertEquals('https://example.org/subdir/next/index.html', $queue->next());
+        $this->assertNull($queue->next());
+    }
+
+    /**
+     * @test
+     */
+    public function processSrcsetAttributes() {
+        $processor = $this->htmlProcessor;
+        $queue = $this->queue;
+
+        $asset = $this->createAsset('/subdir/index.html', $this->htmlBody('<img srcset="/img/2x.png 2x, /img/4x.png 4x">'));
+        $queue->add($asset->getUrl());
+        $html = $processor->process($asset, $queue);
+
+        $this->assertEquals($this->htmlBody('<img srcset="https://example.org/img/2x.png 2x, https://example.org/img/4x.png 4x">'), $html->getContent());
+        $this->assertEquals('https://example.org/subdir/index.html', $queue->next());
+        $this->assertEquals('https://example.org/img/2x.png', $queue->next());
+        $this->assertEquals('https://example.org/img/4x.png', $queue->next());
+        $this->assertNull($queue->next());
+    }
+
+    /**
+     * @test
+     */
+    public function processStyleAttributes() {
+        $processor = $this->htmlProcessor;
+        $queue = $this->queue;
+
+        $asset = $this->createAsset('/subdir/index.html', $this->htmlBody('<div style="background-image: url(../img/other.jpeg)"></div>'));
+        $queue->add($asset->getUrl());
+        $html = $processor->process($asset, $queue);
+
+        $this->assertEquals($this->htmlBody('<div style="background-image: url(https://example.org/img/other.jpeg)"></div>'), $html->getContent());
+        $this->assertEquals('https://example.org/subdir/index.html', $queue->next());
+        $this->assertEquals('https://example.org/img/other.jpeg', $queue->next());
+        $this->assertNull($queue->next());
+    }
+
+    /**
+     * @param string $path
+     * @param string $html
+     * @return Asset
+     */
+    private function createAsset($path, $html) {
+        return new DummyAsset('https://example.org'.$path, 200, 'text/html', $html);
+    }
+
+    /**
+     * @param string $head
+     * @return string
+     */
+    private function htmlHead($head) {
+        return "<!DOCTYPE html>\n<html><head>$head</head><body></body></html>\n";
+    }
+
+    /**
+     * @param string $body
+     * @return string
+     */
+    private function htmlBody($body) {
+        return "<!DOCTYPE html>\n<html><head></head><body>$body</body></html>\n";
+    }
+}
